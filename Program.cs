929ce39d0520aff3b31872403f5b7196e8bad336
Rob <robbryan42@gmail.com>
2020-09-23 15:11:16 -0700

    Initial

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FarmAverages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Drops = "How many drops did your farm get from your afk session?";
+            Console.WriteLine(Drops);
+            double drops = Convert.ToDouble(Console.ReadLine());
+            string Hours = "How many hours were you afk?";
+            Console.WriteLine(Hours);
+            double hours = Convert.ToDouble(Console.ReadLine());
+            double result = drops / hours;
+            Console.WriteLine("Your farm produced an average of " + result + " drops per hour.");
+            Console.WriteLine("Press any key to exit program:");
+            Console.ReadKey();
+        }
+    }
+}
